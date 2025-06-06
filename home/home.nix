791ed1f5d804c7e61d944e554bc9674d46e337c2
nomispaz <nomispaz@example.org>
2025-06-06 08:10:19 +0200

    added alacritty and git to home-manager

--- home/home.nix
+++ home/home.nix
@@ -1,23 +1,64 @@
 { config, pkgs, ... }:
+let
+  username = "simonheise";
+  homeDirectory = "/home/simonheise";
+in
 {
 
   home = {
-    username = "simonheise";
-    homeDirectory = "/home/simonheise";
+    username = "${username}";
+    homeDirectory = "${homeDirectory}";
     stateVersion = "25.05";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.git = {
-    enable = true;
-    userName = "nomispaz";
-    userEmail = "nomispaz@example.org";
-    extraConfig = {
-      credential.helper = "${
-          pkgs.git.override { withLibsecret = true; }
-        }/bin/git-credential-libsecret";
-    };
-  };
+  #####################################################################
+  # Git
+  #####################################################################
+  
+  # allow saving of git credentials in keyring by activating "withLisecret"
+  home.file."${homeDirectory}/.config/git/config".text = ''
+  [credential]
+	helper = "${
+    pkgs.git.override { withLibsecret = true; }
+    }/bin/git-credential-libsecret";
+
+  [user]
+	email = "nomispaz@example.org"
+	name = "nomispaz"
+  '';
+
+  #####################################################################
+  # Alacritty
+  #####################################################################
+  
+  home.file."${homeDirectory}/.config/alacritty/alacritty.toml".text = ''
+    [font]
+    size = 16.0
+
+    [font.bold]
+    family = "DejaVu Sans Mono"
+    style = "Bold"
+    
+    [font.bold_italic]
+    family = "DejaVu Sans Mono"
+    style = "Bold Italic"
+    
+    [font.italic]
+    family = "DejaVu Sans Mono"
+    style = "Italic"
+    
+    [font.normal]
+    family = "DejaVu Sans Mono"
+    style = "Regular"
+    
+    [general]
+    live_config_reload = true
+    
+    [terminal.shell]
+    program = "fish"
+  '';
+  
 }
--- modules/gnome_keyring.nix
+++ modules/gnome_keyring.nix
@@ -5,6 +5,6 @@
   ];
 
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.gnome.enableGnomeKeyring = true;
+  security.pam.services.gdm.enableGnomeKeyring = true;
 
 }
